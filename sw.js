2ffe44a646edd64e606c3715754cda06317654a2
m33tp-6338 <m33tpanchal@gmail.com>
2026-09-03 16:47:06 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "works-tracker-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== "GET" || url.hostname.endsWith("supabase.co")) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.ok && url.origin === self.location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(hit => hit || caches.match("./index.html")))
+  );
+});
